83b2a427809b7bd1b39d5f0c6b884e883bb60922
nguyenh01 <huunguyen199999@gmail.com>
2021-05-06 17:07:29 +0700

    Update

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,8 @@
-const {combineReducers} = redux
+const {combineReducers} = Redux
+import userReducer from './user.reducers'
 
 const rootReducer = combineReducers({
-
+  user: userReducer
 })
 
 export default rootReducer
