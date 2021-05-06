7d31a3b538b90125975b19fb6cfd5a4ddf7a4d65
hoanghuy-99 <vuhoanghuy29101999@gmail.com>
2021-05-06 20:32:23 +0700

    finished comment api

---
+++ server/controllers/comment.js
@@ -0,0 +1,26 @@
+const Comment = require('../models/Comment')
+
+exports.editComment = async (req, res)=>{
+    const comment = await Comment.findByIdAndUpdate(req.params.id, req.body)
+
+    res.json({
+        code: 0,
+        data:{
+            id: comment._id,
+            content: comment.content,
+            author:{
+                id: comment.author,
+                role: comment.authorRole,
+                name: comment.authorRole === 'student' ? (await Student.findById(post.author)).name : (await User.findById(post.author)).name
+            }
+        }
+    })
+}
+
+exports.deleteComment = async (req, res)=>{
+    await Comment.findByIdAndDelete(req.params.id)
+    res.json({
+        code: 0,
+        message: 'Xóa thành công'
+    })
+}
--- server/routers/post.js
+++ server/routers/post.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 
 const postCtrl = require('../controllers/post')
-const {requireToken} = require('../middlewares/authorization')
+const {requireToken, requireYour} = require('../middlewares/authorization')
+const Post = require('../models/Post')
 
 router.get('/posts', requireToken(), postCtrl.getManyPost)
 router.post('/posts',requireToken(), postCtrl.createPost)
 router.get('/posts/:id', requireToken(), postCtrl.getSinglePost)
-router.patch('/posts/:id', requireToken(), postCtrl.editPost)
-router.delete('/posts/:id', requireToken(), postCtrl.deletePost)
+router.patch('/posts/:id', requireToken(), requireYour(Post), postCtrl.editPost)
+router.delete('/posts/:id', requireToken(), requireYour(Post), postCtrl.deletePost)
 router.post('/posts/:id/comments', requireToken(), postCtrl.commentPost)
 
 module.exports = router
