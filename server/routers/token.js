1455da7d8fb739cc8b87459e063273f2950dccf1
hoanghuy-99 <vuhoanghuy29101999@gmail.com>
2021-05-07 20:35:37 +0700

    Update token.js

--- server/routers/token.js
+++ server/routers/token.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 const tokenCtrl = require('../controllers/token')
 router.put('/tokens', tokenCtrl.createTokenByGoogle)
-
+router.post('/tokens', tokenCtrl.createToken)
 module.exports = router
