6cff2c0796b9a66b909473571846160cd524f246
hoanghuy-99 <vuhoanghuy29101999@gmail.com>
2021-05-08 18:45:31 +0700

    add route

--- server/routers/notification.js
+++ server/routers/notification.js
@@ -1,3 +1,12 @@
 const router = require('express').Router()
+const Notification = require('../models/Notification')
+const notificationCtrl = require('../controllers/notification')
+
+router.get('/notifications', requireToken(), notificationCtrl.createNotification)
+router.post('/notifications',requireToken(['teacher']), notificationCtrl.createNotification)
+router.get('/notifications/:id', requireToken(), notificationCtrl.getSingleNotification)
+router.patch('/notifications/:id', requireToken(['teacher']), requireYour(Notification), notificationCtrl.editNotification)
+router.delete('/notifications/:id', requireToken(['teacher']), requireYour(Notification), notificationCtrl.deleteNotification)
+
 
 module.exports = router
