5cf9c2f293e95e54c3a95b0b27fa53c9f58f3f89
tuankiet15021999 <56707074+tuankiet15021999@users.noreply.github.com>
2021-05-12 03:48:22 +0700

    4h sáng vẫn làm bài. Đỉnh cao

--- client/src/page/Modal_Edit_Post/index.js
+++ client/src/page/Modal_Edit_Post/index.js
@@ -1,10 +1,14 @@
+import { requestPostById } from "../../services/post.services"
+
 const {useDispatch,useSelector} = ReactRedux
 const {useState,useEffect} = React
-import {newPost} from '../../redux/actions/post.actions'
-const Modal_Post = ()=>{
+const Modal_Edit_Post = (props)=>{
+    const id = props
     const dispatch = useDispatch()
+    const postById = JSON.stringify(requestPostById(id.props.id))
+    console.log("data",postById);
     function closeModal(){
-        document.getElementById('modal_change_avatar').style.display='none'
+        document.getElementById(id.props.edit).style.display='none'
     }
     function activeImage() {
         const defautBtnPic = document.querySelector("#pic-file")
@@ -12,7 +16,7 @@ const Modal_Post = ()=>{
     }
     const handleChange = ()=>{
         const uploadAvatarFile = document.getElementById("pic-file")
-        const avatar = document.getElementById("img_modal_post")
+        const avatar = document.getElementById("img_modal_edit_post")
           if(uploadAvatarFile.value){
             const img = event.target.files[0];
             const imgUrl = URL.createObjectURL(img);
@@ -28,7 +32,7 @@ const Modal_Post = ()=>{
     const [disableVid,setDisableVid] = useState(false)
     const [disableImg,setDisableImg] = useState(false)
     const handleChangeVideo = (e) =>{
-        const uploadVideo = document.getElementById("youtube_embed")
+        const uploadVideo = document.getElementById("youtube_edit_embed")
         const url_ytb = e.target.value
         const test ="https://www.youtube.com/embed/"+url_ytb.slice(32)+"?controls=1"
         setUrlYTB(test)
@@ -39,28 +43,26 @@ const Modal_Post = ()=>{
             setDisableImg(false)
         }
     }
-    const addPost = () => {
-        const content = document.getElementById('value_post').value
-        const link_ytb = document.getElementById('youtube_embed').getAttribute('src')
-        console.log(content,link_ytb);
-        dispatch(newPost(content,link_ytb))
+    const editPost = () => {
+        const content = document.getElementById('value_edit_post').value
+        const link_ytb = document.getElementById('youtube_edit_embed').getAttribute('src')
         closeModal()
     }
     const handleCancelImg = () =>{
-        document.getElementById("img_modal_post").setAttribute('src','')
+        document.getElementById("img_modal_edit_post").setAttribute('src','')
         document.getElementById('btn_cancel_img').style.display ='none'
         setHiddenImg(true)
         setDisableVid(false)
     }
     return(
-        <div id="modal_change_avatar" className="w3-modal w3-animate-opacity modal_post">
+        <div id={id.props.edit} className="w3-modal w3-animate-opacity modal_post">
                 <div className="w3-modal-content modal_post_content">
                   <div className="w3-container w3-teal"> 
                     <span onClick={closeModal}
                     className="w3-button w3-display-topright"><i className="fas fa-times" id="btn_close_modal"></i></span>
-                    <center><h2>Tạo bài đăng</h2></center>
+                    <center><h2>Chỉnh sửa bài đăng</h2></center>
                 </div>
-                <div className="w3-container">
+                    <div className="w3-container">
                     <div className="row">
                         <div className="col-lg-1">
                             <img src="/img/avatar.jpg" id="avatar_post"/>
@@ -70,18 +72,18 @@ const Modal_Post = ()=>{
                         </div>
                     </div>
                     <div>
-                        <textarea placeholder="Bạn đang nghĩ gì?" id="value_post"></textarea>
+                        <textarea placeholder="Bạn đang nghĩ gì?" id="value_edit_post"></textarea>
                     </div>
                     <div className="form-group">
                         <input onChange={handleChangeVideo} type="text" disabled={disableVid} className="form-control" id="embed_video" 
                         placeholder="Thêm đường dẫn youtube"/>
                     </div>
                     <div>
-                        <img src="" id="img_modal_post" hidden={hiddenImg}/>
+                        <img src="" id="img_modal_edit_post" hidden={hiddenImg}/>
                         <button id="btn_cancel_img" onClick={handleCancelImg}><i className="fas fa-times"/></button>
                     </div>
                     <div>
-                        <iframe id="youtube_embed" width="560" height="315" src={urlYTB} allowFullScreen hidden={hiddenVid} 
+                        <iframe id="youtube_edit_embed" width="560" height="315" src={urlYTB} allowFullScreen hidden={hiddenVid} 
                         ></iframe>
                     </div>
                     <div className="row" id="div_modal_post_social">
@@ -94,7 +96,7 @@ const Modal_Post = ()=>{
                     <hr/>
                     <div className="row">
                         <div className="col-lg-12">
-                        <button id="btn_modal_post" onClick={addPost} className="btn btn-primary">Đăng</button>
+                        <button id="btn_modal_post" onClick={editPost} className="btn btn-primary">Đăng</button>
                         </div>
                     </div>
                 </div>
@@ -102,4 +104,4 @@ const Modal_Post = ()=>{
         </div>
     )
 }
-export default Modal_Post
+export default Modal_Edit_Post
--- client/src/page/Navigation/Navigation.js
+++ client/src/page/Navigation/Navigation.js
@@ -1,7 +1,20 @@
 const { Link, Route, BrowserRouter, Switch } = ReactRouterDOM;
+const {useState,useEffect} = React 
 import {getRole} from '../../cookie'
 const Navigation = ({children}) =>{
-    console.log(getRole()); 
+    var hiddenStudent = true
+    var hiddenTeacher = true
+    var hiddenAdmin = true
+    console.log(getRole());
+    if(getRole() == "admin"){
+        hiddenAdmin = false;
+    }
+    else if(getRole() == "teacher"){
+        hiddenTeacher = false;   
+    }
+    else{
+        hiddenStudent = false
+    }
     return(
         <div className="row">
                 <div className="col-12 col-lg-2" id="navigation_div">
@@ -11,10 +24,10 @@ const Navigation = ({children}) =>{
                     <div className="list-group">
                         <Link to="/home" className="list-group-item list-group-item-action"><i className="fas fa-home"></i>Trang chủ</Link>
                         <Link to="/notification" className="list-group-item list-group-item-action"><i className="fas fa-bell"></i>Thông báo</Link>
-                        <Link to="/facultyInfo" className="list-group-item list-group-item-action"><i className="fas fa-key"></i>Đổi mật khẩu</Link>
-                        <Link to="/student" className="list-group-item list-group-item-action"><i className="fas fa-user"></i>Đổi thông tin</Link>
-                        <Link to="/faculty" className="list-group-item list-group-item-action"><i className="fas fa-tasks"></i>Quản lý thông báo</Link>
-                        <Link to="/admin" className="list-group-item list-group-item-action"><i className="fas fa-user-plus"></i>Tạo tài khoản Phòng/Ban</Link>
+                        <Link to="/facultyInfo" hidden={hiddenTeacher} className="list-group-item list-group-item-action"><i className="fas fa-key"></i>Đổi mật khẩu</Link>
+                        <Link to="/student" hidden={hiddenStudent} className="list-group-item list-group-item-action"><i className="fas fa-user"></i>Đổi thông tin</Link>
+                        <Link to="/faculty" hidden={hiddenTeacher} className="list-group-item list-group-item-action"><i className="fas fa-tasks"></i>Quản lý thông báo</Link>
+                        <Link to="/admin" hidden={hiddenAdmin} className="list-group-item list-group-item-action"><i className="fas fa-user-plus"></i>Tạo tài khoản Phòng/Ban</Link>
                     </div>
                 </div>
                 <div className=" col-12 col-lg-10" id="body_div">
