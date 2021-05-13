b395dd363663e00c6d1a1ccc8e5927685ceca73a
hoanghuy-99 <vuhoanghuy29101999@gmail.com>
2021-05-13 13:26:54 +0700

    Update comment.js

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,8 +1,8 @@
 const Comment = require('../models/Comment')
+const Post = require('../models/Post')
 
 exports.editComment = async (req, res)=>{
     const comment = await Comment.findByIdAndUpdate(req.params.id, {content:req.body.content})
-
     res.json({
         code: 0,
         data:{
@@ -21,6 +21,7 @@ exports.editComment = async (req, res)=>{
 
 exports.deleteComment = async (req, res)=>{
     await Comment.findByIdAndDelete(req.params.id)
+    await Post.updateOne({comments: req.params.id}, {$pull: {comments: req.params.id}})
     res.json({
         code: 0,
         message: 'Xóa thành công'
