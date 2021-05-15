ef95fe01836539c76ee78c3edcd71a2974a48b40
nguyenh01 <huunguyen199999@gmail.com>
2021-05-15 14:47:45 +0700

    Update

--- client/src/services/token.services.js
+++ client/src/services/token.services.js
@@ -1,5 +1,6 @@
 var host = window.location.protocol + "//" + window.location.host
 host = 'http://localhost:8080'
+
 async function requestToken(username,password) {
     const requestOptions = {
         method: 'POST',
