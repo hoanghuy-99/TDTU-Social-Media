eac7bfa8303629e5da4120234cb964ed1cfb8b60
hoanghuy-99 <vuhoanghuy29101999@gmail.com>
2021-05-16 21:30:10 +0700

    fix

--- server/routers/notification.js
+++ server/routers/notification.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const Notification = require('../models/Notification')
 const notificationCtrl = require('../controllers/notification')
 const { requireToken, requireYour } = require('../middlewares/authorization')
+const { useValidator } = require('../middlewares/validator')
+const { NotificationCreateValidator, NotificationEditValidator } = require('../validators/notification')
 
 router.get('/notifications', requireToken(), notificationCtrl.getManyNotification)
-router.post('/notifications',requireToken(['teacher']),notificationCtrl.createNotification)
+router.post('/notifications',requireToken(['teacher']), useValidator(NotificationCreateValidator),notificationCtrl.createNotification)
 router.get('/notifications/:id', requireToken(), notificationCtrl.getSingleNotification)
-router.patch('/notifications/:id', requireToken(['teacher']), requireYour(Notification),notificationCtrl.editNotification)
+router.patch('/notifications/:id', requireToken(['teacher']), useValidator(NotificationEditValidator),requireYour(Notification),notificationCtrl.editNotification)
 router.delete('/notifications/:id', requireToken(['teacher']), requireYour(Notification), notificationCtrl.deleteNotification)
 
 
